1563b1b849dc670add848eab26623e3507e6e406
ohyootaek <cariros@naver.com>
2024-10-08 01:32:32 +0900

    baseUrl 수정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const accessToken = localStorage.getItem('accessToken');
 
 const axiosInstance = axios.create({
-    baseURL: 'http://15.164.246.46:8080',
+    baseURL: 'http://cariros.store:8080',
     timeout: 2000,
     headers: {
         "content-type": "application/json",
