93929262dd05e7c95879e5cfa872a3c7caa96290
ohyootaek <cariros@naver.com>
2024-10-07 22:58:21 +0900

    오류코드 수정

--- src/pages/user/Login.js
+++ src/pages/user/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import usePage from "../../pages/user/usePage";
 import "../../styles/login.css";
